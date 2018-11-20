f6cb759d41af7ae83a89be0e515a9a5dc531ef37
nicolajfirst <nicolajfirst@gmail.com>
2018-11-20 18:47:36 +0200

    project with map

--- 03-profidom/app/js/main.js
+++ 03-profidom/app/js/main.js
@@ -1,51 +1,15 @@
-$( document ).ready(function() {
 
-    $('.js-slider').slick({
-        prevArrow: '.js-left svg',
-        nextArrow: '.js-right svg',
-        dots: false,
-        infinite: true,
-        arrows: true,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
+    // Initialize and add the map
+    function initMap() {
+    // The location of Uluru
+    var uluru = {lat: 46.4768919, lng: 30.7278724};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+    document.getElementById('map'),
+        {
+        zoom: 14, center: uluru
+        });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+}
 
-    // $('input[name=email]').val('Your Email*');
-
-    $('.js-push-btn').click(function () {
-        $('.js-overlay').fadeIn();
-        $('.js-main').css('filter', 'blur(5px)');
-    });
-
-    $('.js-close').click(function () {
-        $('.js-overlay').fadeOut();
-        $('.js-main').css('filter', 'none');
-    });
-
-    $('.js-btn').click(function () {
-        $('.js-overlay').fadeOut();
-        $('.js-main').css('filter', 'none');
-    });
-
-    // Animation on scroll
-
-    $('.js-team').waypoint(function(direction) {
-        $('.js--wp-1').addClass('animated  slideInUp');
-    });
-
-    $('.js-team').waypoint(function(direction) {
-        $('.js--wp-2').addClass('animated lightSpeedIn');
-    });
-
-    $('.js-form').waypoint(function(direction) {
-        $('.js--wp-3').addClass('animated lightSpeedIn');
-    });
-
-    $('.js-form').waypoint(function(direction) {
-        $('.js--wp-4').addClass('animated zoomInUp');
-    });
-
-
-
-});
