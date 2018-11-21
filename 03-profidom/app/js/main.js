4dbde1c282b7129a6bc896ba0c61755985b62279
nicolajfirst <nicolajfirst@gmail.com>
2018-11-21 14:46:39 +0200

    Page without adaptive

--- 03-profidom/app/js/main.js
+++ 03-profidom/app/js/main.js
@@ -2,14 +2,15 @@
     // Initialize and add the map
     function initMap() {
     // The location of Uluru
-    var uluru = {lat: 46.4768919, lng: 30.7278724};
+    var uluru = {lat: 46.4776966, lng: 30.7549346};
     // The map, centered at Uluru
     var map = new google.maps.Map(
     document.getElementById('map'),
         {
-        zoom: 14, center: uluru
+        zoom: 10, center: uluru
         });
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: uluru, map: map});
 }
+
 
